4606ed90f22609770d46c3e1df9d5055cb39cc3d
Jacob Whitehead <j.whitehead143@hotmail.co.uk>
2022-04-03 17:09:08 +0100

    Updates to FTP server

--- src/server.js
+++ src/server.js
@@ -1,12 +1,46 @@
 const { FtpSrv, FileSystem } = require('ftp-srv');
+const { Netmask } = require('netmask');
+const { networkInterfaces } = require('os');
+const bunyan = require('bunyan');
+
+const nets = networkInterfaces();
+const logger = bunyan.createLogger({ name: 'ftp-ipfs' });
 const [username, password] = process.argv.slice(2);
 
+const getNetworks = () => {
+  let networks = {};
+  for (const name of Object.keys(nets)) {
+    for (const net of nets[name]) {
+      if (net.family === 'IPv4' && !net.internal) {
+        networks[net.address + "/24"] = net.address
+      }
+    }
+  }
+  return networks;
+};
+
+const resolverFunction = (ip) => {
+  const networks = getNetworks();
+  for (const network in networks) {
+    if (new Netmask(network).contains(ip)) {
+      return networks[network];
+    }
+  }
+  return "127.0.0.1";
+};
+
 const startFtpServer = async (username, password) => {
-  const server = new FtpSrv();
+  const server = new FtpSrv({
+    pasv_url: resolverFunction,
+    url: 'ftp://127.0.0.1:21',
+    log: logger
+  });
   
   server.on('login', (data, resolve, reject) => {
-    if(data.username === username && data.password === password){
-      return resolve({ root: "/ftp-ipfs" });    
+    if(data.username === username && data.password === password) {
+      console.log('Authentication successful');
+      // return resolve({ root: "./ftp-drive" });
+      return resolve();    
     }
     return reject(new errors.GeneralError('Invalid username or password', 401));
   });
@@ -17,17 +51,17 @@ const startFtpServer = async (username, password) => {
   
   // File downloaded
   server.on('RETR', (error, filePath) => {
-    console.log('DOWNLOAD', filePath)
+    console.log('DOWNLOAD');
   });
   
   // File uploaded
   server.on('STOR', (error, fileName) => {
-    console.log('UPLOAD', fileName)
+    console.log('UPLOAD');
   });
   
   // File name changed
   server.on('RNTO', (error, fileName) => {
-    console.log('RENAME', fileName)
+    console.log('RENAME');
   });
   
   const closeFtpServer = async () => { 
